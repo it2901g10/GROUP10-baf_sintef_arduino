78737959b0b2dc17fd015417ae76473e920afab3
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-01-25 15:32:02 +0100

    Changed @author

--- java/src/debug/Log.java
+++ java/src/debug/Log.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package debug;
 
 import java.util.ArrayList;
@@ -11,7 +7,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author xolan
+ * @author Jonas Svarvaa
  */
 public class Log {
     
