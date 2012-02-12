066b4369d8372573fd6c8a0af4079e0797d064aa
lemrey <lemrey@deadbeef.localdomain>
2012-02-12 23:19:00 +0100

    deleted:    AndroidManifest.xml 	modified:   README.md 	new file:   libs/json_simple-1.1.jar 	new file:   libs/opensocial-1.0.jar 	new file:   src/libs/json_simple-1.1.jar 	new file:   src/libs/opensocial-1.0.jar 	deleted:    src/no/ntnu/osnap/arduinointerface/ComLayerListener.java 	deleted:    src/no/ntnu/osnap/arduinointerface/Protocol.java 	deleted:    src/no/ntnu/osnap/bluetooth/ArduinoBluetoothAdapter.java 	deleted:    src/no/ntnu/osnap/bluetooth/ArduinoBluetoothAdapterListener.java 	deleted:    src/no/ntnu/osnap/bluetooth/ArduinoBluetoothConnection.java 	new file:   src/no/ntnu/osnap/social/Receiver/Receiver.java 	new file:   src/no/ntnu/osnap/social/Sender/Sender.java 	new file:   src/no/ntnu/osnap/social/Util.java 	new file:   src/no/ntnu/osnap/social/receiver/Receiver.java 	new file:   src/no/ntnu/osnap/social/sender/Sender.java 	deleted:    src/no/ntnu/osnap/test/AndroidBluetoothTest.java 	deleted:    src/sna/fb/BaseDialogListener.java 	deleted:    src/sna/fb/BaseRequestListener.java 	deleted:    src/sna/fb/LoginButton.java 	deleted:    src/sna/fb/MainActivity.java 	deleted:    src/sna/fb/SessionEvents.java 	deleted:    src/sna/fb/SessionStore.java 	deleted:    src/test.java
    deleted some junk, added
    no.ntnu.osnap.social
    no.ntnu.osnap.social.sender
    no.ntnu.osnap.social.receiver

--- java/src/no/ntnu/osnap/bluetooth/ArduinoBluetoothAdapterListener.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.osnap.bluetooth;
-
-import java.util.ArrayList;
-
-public interface ArduinoBluetoothAdapterListener {
-	public void arduinoDeviceFound(ArduinoBluetoothConnection arduinoDevice);
-	public void scanComplete(ArrayList<String> arduinoDevicesFound);
-}
