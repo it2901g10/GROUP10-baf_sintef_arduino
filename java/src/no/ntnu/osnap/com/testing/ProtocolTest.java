b2c262421b7564bd093d33bacc26e7708012ea61
zefz <jnsn.johan@gmail.com>
2012-02-17 15:36:26 +0100

    added the Apache 2.0 license

--- java/src/no/ntnu/osnap/com/testing/ProtocolTest.java
+++ java/src/no/ntnu/osnap/com/testing/ProtocolTest.java
@@ -1,7 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* Copyright 2012 NTNU
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
 package testing;
 
 import java.text.SimpleDateFormat;
@@ -10,10 +21,6 @@ import javax.swing.text.DateFormatter;
 import source.ComLayer;
 import source.Protocol;
 
-/**
- *
- * @author anders
- */
 public class ProtocolTest {
     public static void main(String[] args) {
         Protocol arduino = new Protocol(new ComLayer());
