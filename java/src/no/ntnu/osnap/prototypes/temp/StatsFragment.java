564067020598031319032c271a6676bc50628697
Bjornar Wold <bhwold@gmail.com>
2012-03-12 15:03:41 +0100

    added temp proto, sorry for folder mess

---
+++ java/src/no/ntnu/osnap/prototypes/temp/StatsFragment.java
@@ -0,0 +1,19 @@
+package no.ntnu.osnap.temp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * @author B
+ */
+public class StatsFragment extends Fragment {
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+    Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.stats_fragment, container, false);
+   }
+}
