34b8b1b74da149c8f216e2b2279278c38b2fd442
Anders Eie <strykejernene@gmail.com>
2012-04-20 18:05:03 +0200

    Fixes in ComLib

--- java/src/no/ntnu/osnap/com/testing/ProtocolTest.java
+++ java/src/no/ntnu/osnap/com/testing/ProtocolTest.java
@@ -4,27 +4,38 @@ import java.util.concurrent.TimeoutException;
 import no.ntnu.osnap.com.deprecated.ComLayer;
 
 public class ProtocolTest {
-    public static void main(String[] args) throws TimeoutException {
+    public static void main(String[] args) throws TimeoutException, InterruptedException {
         ComLayer arduino = new ComLayer();
-		arduino.setDaemon(true);
+		//arduino.setDaemon(true);
 		arduino.start();
 		
 		boolean toggle = false;
         
-        for (long i = 0; i < 10000000; ++i){
+        for (long i = 0; i < 1000; ++i){
             //arduino.print(new String(new char[250]));
             //arduino.newPrint(new SimpleDateFormat("HH:mm:ss").format(new Date()));
 			//arduino.pulse(13);
 			//int value = arduino.sensor(0);
             //System.out.println("value: " + value);
 			//arduino.write(13, value);
-            //arduino.print(i + "s");
+            arduino.print(i + "s");
+			arduino.print(i + "s", true);
+			
+			//Thread.sleep(100);
 			
 			//arduino.print(value + "   ", false);
-			System.out.println("0: " + arduino.sensor(0));
-			System.out.println("10: " + arduino.sensor(10));
-			System.out.println("20: " + arduino.sensor(20));
+			//System.out.println("0: " + arduino.sensor(0));
+			//System.out.println("10: " + arduino.sensor(10));
+			//System.out.println("20: " + arduino.sensor(20));
         }
+		
+		Thread.sleep(10000);
+		arduino.stopThread();
+		
+		while (arduino.isAlive())
+			Thread.sleep(100);
+		
+		arduino.close();
         
         //arduino.print("ZooPark");
         //arduino.print("D");
