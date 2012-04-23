78b3a8fd7d89b4735b2b67d5a665d26f75000caa
Henrik Goldsack <henrik0312@gmail.com>
2012-04-23 05:11:40 +0200

    Added basic TshirtApp

---
+++ java/TshirtApp/src/no/ntnu/osnap/tshirt/ActivityStartWindow.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.ntnu.osnap.tshirt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.R;
+
+public class ActivityStartWindow extends Activity
+{
+
+    TshirtSingleton singleton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_window);
+        singleton = TshirtSingleton.getInstance(this);
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        Button setRulesButton = (Button)findViewById(R.id.sw_buttonSetRules);
+        setRulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ActivityStartWindow.this, ActivityRulesList.class);
+                startActivity(i);
+            }
+        });
+        
+        Button arduinoConnectionButton = (Button)findViewById(R.id.sw_buttonConnection);
+        arduinoConnectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                singleton.toggleArduinoConnection();
+            }
+        });
+
+        Button searchSSButton = (Button)findViewById(R.id.sw_buttonSearchSocialServices);
+        searchSSButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] socialServices = singleton.searchSocialServices();
+
+            }
+        });
+        
+    }
+
+}
